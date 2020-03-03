6bc8848f5392a22280c2d9fa670efdff4d2bb856
annastepanova <annastepanova1811@gmail.com>
2020-03-03 15:30:53 -0500

    built socket.io instance

--- server/index.js
+++ server/index.js
@@ -13,6 +13,12 @@ const io = socketio(server)
 io.on('connection', (socket) => {
   console.log('We have a new connection')
 
+  socket.on('join', ({ name, room }, callback) => {
+    console.log(name, room)
+
+    callback()
+  })
+
   socket.on('disconnect', () => {
     console.log('User had left')
   })
