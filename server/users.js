dc7b6a7a952118f8891f3082a330aa168ce516bd
annastepanova <annastepanova1811@gmail.com>
2020-03-03 16:09:44 -0500

    addUser function

---
+++ server/users.js
@@ -0,0 +1,20 @@
+const users = []
+
+const addUser = ({ id, name, room }) => {
+  
+  name = name.trim().toLowerCase()
+  room = room.trim().toLowerCase()
+}
+
+const removeUser = () => {
+
+
+}
+
+const getUser = () => {
+
+}
+
+const getUsersInRoom = () => {
+
+}
