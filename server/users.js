422b22bb53deb2752e3418bddf6456f1dc391fd5
annastepanova <annastepanova1811@gmail.com>
2020-03-03 16:16:09 -0500

    addUser function

--- server/users.js
+++ server/users.js
@@ -4,6 +4,19 @@ const addUser = ({ id, name, room }) => {
   
   name = name.trim().toLowerCase()
   room = room.trim().toLowerCase()
+
+  const existingUser = users.find((user) => user.room === room && user.name === name)
+
+  if(existingUser) {
+    return {error: 'Username is taken'}
+  }
+
+  const user = { id, name, room }
+
+  users.push(user)
+
+  return user
+
 }
 
 const removeUser = () => {
