46836c7d906ce582137fc2ed3b8cdf7da2d192fd
annastepanova <annastepanova1811@gmail.com>
2020-03-07 22:19:54 -0500

    created Input component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import queryString from 'query-string'
 import io from 'socket.io-client'
 import InfoBar from './InfoBar'
+import Input from './Input'
 import './Chat.css'
 
 let socket
@@ -49,7 +50,7 @@ const Chat = ({location}) => {
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room}/>
-        <input value={message} onChange={(event) => setMessage(event.target.value)} onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}/>
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
     </div>
   )
