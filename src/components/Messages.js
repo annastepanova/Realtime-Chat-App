10606baf7ed051fa625c1c187df44487729c1284
annastepanova <annastepanova1811@gmail.com>
2020-03-08 20:06:58 -0400

    css for Messages component

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -5,7 +5,7 @@ import Message from './Message'
 import './Messages.css'
 
 const Messages = ({messages, name}) => (
-  <ScrollToBottom>
+  <ScrollToBottom className="messages">
     {messages.map((message, index) => <div key={index}><Message message={message} name={name}/></div>)}
 
   </ScrollToBottom>
