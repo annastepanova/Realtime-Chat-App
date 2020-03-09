b216b6ad54b1a3f6a947c6ed7ff298abbdae8de5
annastepanova <annastepanova1811@gmail.com>
2020-03-09 18:18:51 -0400

    added react-emoji

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import ReactEmoji from 'react-emoji'
 import './Message.css'
 
 const Message = ({ message: { user, text }, name }) => {
@@ -16,7 +16,7 @@ const Message = ({ message: { user, text }, name }) => {
       <div className="messageContainer justifyEnd">
         <p className="sentText pr-10">{trimmedName}</p>
         <div className="messageBox backgroundBlue">
-          <p className="messageText colorWhite">{text}</p>
+          <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
         </div>
       </div>
     ) : 
