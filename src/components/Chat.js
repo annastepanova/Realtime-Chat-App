acbaa0e364a1bf971bc0f45b0f8dcf48b693cf7a
annastepanova <annastepanova1811@gmail.com>
2020-06-24 17:00:47 -0400

    modified readme file, added favicon

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -16,7 +16,7 @@ const Chat = ({location}) => {
   const [message, setMessage] = useState('')
   const [messages, setMessages] = useState([])
 
-  const ENDPOINT = 'localhost:5000'
+  const ENDPOINT = 'http://localhost:5000'
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search)
